d9c4704b81b553121f6a4d48e6380f270a793191
parth-cykero <parth@cykero.eu>
2025-07-12 14:15:50 +0530

    feat: update index file to include Lumpsum, Compound Interest, Goal-Based SIP, and SWP calculators

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,2 +1,6 @@
 export * from './LoanCalculator';
 export * from './SipCalculator';
+export * from './LumpsumSipCalculator';
+export * from './CompoundInterestCalculator';
+export * from './GoalBasedSipCalculator';
+export * from './SwpCalculator';
